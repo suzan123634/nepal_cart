27bbf24a78908839b6c7d8395d5c0f7fd04af016
unknown <sonygurung275@gmail.com>
2019-10-30 21:58:37 +0530

    first commit

---
+++ nepal_cart/urls.py
@@ -0,0 +1,49 @@
+"""nepal_cart URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from django.views.static import serve
+from django.contrib.admin import AdminSite
+
+AdminSite.site_header = "Nepal_Cart"
+AdminSite.site_title = "Nepal_Cart title"
+AdminSite.index_title = "Nepal_Cart title"
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('search', views.search, name='search'),
+    path('about/', views.about, name='about'),
+    path('blog/', views.blog, name='blog'),
+    path('boy/', views.boy, name='boy'),
+    path('boys/', views.boys, name='boys'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('contact/', views.contact, name='contact'),
+    path('faq/', views.faq, name='faq'),
+    path('girl/', views.girl, name='girl'),
+    path('girls/', views.girls, name='girls'),
+    path('men/', views.men, name='men'),
+    path('mens/', views.mens, name='mens'),
+    path('payment/', views.payment, name='payment'),
+    path('shop/', views.shop, name='shop'),
+    path('single/', views.single, name='single'),
+    path('women/', views.women, name='women'),
+    path('womens/', views.womens, name='womens'),
+    path('login/', views.login, name='login'),
+    path('accounts/', include('accounts.urls')),
+]
---
+++ nepal_cart/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, template_name='index.html')
+
+def search(request):
+    return render(request, template_name='index.html')
+
+def about(request):
+    return render(request, template_name='pages/about.html')
+
+def blog(request):
+    return render(request, template_name='pages/blog.html')
+
+def boy(request):
+    return render(request, template_name='pages/boy.html')
+
+def boys(request):
+    return render(request, template_name='pages/boys.html')
+
+def checkout(request):
+    return render(request, template_name='pages/checkout.html')
+
+def contact(request):
+    return render(request, template_name='pages/contact.html')
+
+def faq(request):
+    return render(request, template_name='pages/faq.html')
+
+def girl(request):
+    return render(request, template_name='pages/girl.html')
+
+def girls(request):
+    return render(request, template_name='pages/girls.html')
+
+def men(request):
+    return render(request, template_name='pages/men.html')
+
+def mens(request):
+    return render(request, template_name='pages/mens.html')
+
+def payment(request):
+    return render(request, template_name='pages/payment.html')
+
+def single(request):
+    return render(request, template_name='pages/single.html')
+
+def shop(request):
+    return render(request, template_name='pages/shop.html')
+
+def women(request):
+    return render(request, template_name='pages/women.html')
+
+def womens(request):
+    return render(request, template_name='pages/womens.html')
+
+def login(request):
+    return render(request, template_name='pages/login.html')
